b1363b878e8db28390d63f58a0becd6cf1767f27
Quaomiuzzaman Kabbya <kabbya44@gmail.com>
2025-04-08 20:54:09 +0600

    add-window-sum

--- easy/array/maximum_average_subarray.py
+++ easy/array/maximum_average_subarray.py
@@ -5,8 +5,8 @@ class Solution:
         max_sum = window_sum
 
         for i in range(k, len(nums)):
-            window_sum  += nums[i] - nums[i - k] 
-            max_sum = max(max_sum, window_sum) 
+            window_sum += nums[i] - nums[i - k]
+            max_sum = max(max_sum, window_sum)
 
         
         return max_sum / k
