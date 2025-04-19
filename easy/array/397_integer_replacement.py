1935cc3b42601830ecc6cc9cbc723748f9ad5631
Quaomiuzzaman Kabbya <kabbya44@gmail.com>
2025-04-19 20:24:15 +0600

    integer replacement

--- easy/array/397_integer_replacement.py
+++ easy/array/397_integer_replacement.py
@@ -7,16 +7,22 @@ def integerReplacement(n):
         if n == 1:
             return 0
         
-        if n % 2 == 0:
-           return 1 + operation(n//2)
+        if cache[n]:
+            return cache[n]
         
-        ops1 = operation(n - 1)
-        ops2 = operation(n + 1)
-        return 1 + min(ops1, ops2)
+        if n % 2 == 0:
+            cache[n//2] = operation(n//2)
+            cache[n] = 1 + cache[n//2]
+            return cache[n]
+           
+        cache[n+1] = operation(n - 1)
+        cache[n-1] = operation(n - 1)
+        cache[n] = 1 + min(cache[n+1], cache[n-1])
+        return cache[n]
 
     cache = defaultdict(int)
 
-    return count_ops(n)
+    return operation(n)
 
 n = 7
 
